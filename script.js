2470313ff5e1e43d4a16e1d8fbcaa40ca214f585
thosijulio <thosijulio@gmail.com>
2021-03-22 16:58:10 -0300

    correcao V2 exercicio 7

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function buttonLoginAlert() {
   const buttonLogin = document.getElementById('button-login');
   const emailPhoneInput = document.getElementById('user-email-phone');
-  buttonLogin.addEventListener('click', function () {
+  buttonLogin.addEventListener('click', () => {
     alert(emailPhoneInput.value);
   });
 }
