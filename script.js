bafefc64e4344f7aff21dfb348df3bc6a365edaf
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-09 23:36:47 -0300

    Improving design and estructure application

--- script.js
+++ script.js
@@ -6,4 +6,35 @@ function buttonLoginAlert() {
   });
 }
 
+function selectInputDays() {
+  const selectDays = document.getElementById('birthdate-days');
+  for (let i = 1; i <= 31; i += 1) {
+    const newOption = document.createElement('option');
+    newOption.innerHTML = i;
+    selectDays.appendChild(newOption);
+  }
+}
+
+function selectInputMonths() {
+  const selectMonths = document.getElementById('birthdate-months');
+  for (let i = 1; i <= 12; i += 1) {
+    const newOption = document.createElement('option');
+    newOption.innerHTML = i;
+    selectMonths.appendChild(newOption);
+  }
+}
+
+function selectInputYears() {
+  const selectYears = document.getElementById('birthdate-years');
+  for (let i = 1950; i <= 2021; i += 1) {
+    const newOption = document.createElement('option');
+    newOption.innerHTML = i;
+    selectYears.appendChild(newOption);
+  }
+}
+
+selectInputDays();
+selectInputMonths();
+selectInputYears();
+
 buttonLoginAlert();
